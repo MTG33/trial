04638e77f5c81e0de48b190832d7f2d37bd9a036
MTG33 <119414929+MTG33@users.noreply.github.com>
2022-11-29 17:07:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+MTG33 = Client(
+    name="MTG33",
+    api_id=API_ID
+    api_hash=API_HASH
+    bot_token=BOT_TOKEN
+)
+
+
+print("JOIN MTG33_BOTZ")
+
+MTG33.run()
